bb2946f5bfbec6d1cdfcd28d95431cd4bdcb94f9
GabrielfsCarrasco <152878089+GabrielfsCarrasco@users.noreply.github.com>
2023-12-11 23:09:41 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.h1', {duration: 2000});
+
+sr.reveal('#img', {rotate: {x: 0, y: 80, z:0}, duration: 5500});
+
+sr.reveal('.pg', {rotate: {x: 100, y: 100, z:0}, duration: 3500});
+
+sr.reveal('.im', {rotate: {x: 0, y: 0, z:180}, duration: 2000});
+
+sr.reveal('.fim', {duration: 4000});
+
+sr.reveal('.meses', {duration: 4000});
+
+sr.reveal('.button', {rotate: {x: 0, y: 100, z:0}, duration: 1000});
+
+sr.reveal('#p', {duration: 3000});
+
+sr.reveal('#lista', {duration: 3000});
+
+sr.reveal('.continuando', {duration: 3000});
