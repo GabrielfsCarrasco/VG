49e2c984949df59c1995bb63f66b687e92ff7424
GabrielfsCarrasco <152878089+GabrielfsCarrasco@users.noreply.github.com>
2023-12-18 20:54:24 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -10,20 +10,26 @@ sr.reveal('.im', {rotate: {x: 0, y: 0, z:180}, duration: 2000});
 
 sr.reveal('.fim', {duration: 4000});
 
+sr.reveal('.sou', {duration: 4000});
+
+sr.reveal('.nam', {duration: 5000});
+
+sr.reveal('.fut', {duration: 6000});
+
 sr.reveal('.meses', {duration: 4000});
 
-sr.reveal('.button', {rotate: {x: 0, y: 100, z:0}, duration: 1000});
+sr.reveal('.button', {rotate: {x: 0, y: 100, z:0}, duration: 4000});
+
+sr.reveal('.voltar', {rotate: {x: 0, y: 100, z:0}, duration: 1000});
 
 sr.reveal('#p', {duration: 3000});
 
-sr.reveal('#lista', {duration: 3000});
-
 sr.reveal('.continuando', {duration: 3000});
 
-sr.reveal('.fut', {duration: 3000});
+sr.reveal('.cor', {duration: 4000});
 
-sr.reveal('.sou', {duration: 3000});
+sr.reveal('.ine', {duration: 4000});
 
-sr.reveal('.nam', {rotate: {x: 0, y: 100, z:0}, duration: 1000});
+sr.reveal('.dc', {duration: 2000});
 
-sr.reveal('.voltar', {duration: 1000});
+sr.reveal('.obs', {duration: 7000});
